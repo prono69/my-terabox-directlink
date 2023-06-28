fc419a5cf7c9b3f9edb7cd68814ed770714f7be2
Brainded Kazuma <55076817+prono69@users.noreply.github.com>
2023-06-28 13:07:01 +0530

    YorMom

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,7 +83,7 @@ const Index = () => {
             Get Detail {isLoading['main'] && <Spinner className='ms-2' as="span" animation="border" size="sm" />}
           </Button>
           <br/>
-          <p>Source: <a href="https://github.com/sapthesh/terabox-directlink/">Terabox DL</a></p>
+          <p>Source: <a href="https://github.com/prono69/">Yor Mom</a></p>
         </div>
       </Form>
 
